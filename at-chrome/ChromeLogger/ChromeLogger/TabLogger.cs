0fe1a3f1ac841f8f4677eb7c93212faa32462c46
NGimhana <gimhanadesilva.15@cse.mrt.ac.lk>
2017-09-20 21:15:21 +0530

    Initial Commit

---
+++ at-chrome/ChromeLogger/ChromeLogger/TabLogger.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Automation;
+
+using Newtonsoft.Json;
+
+
+namespace ChromeLogger
+{
+    class JSONLOG
+    {
+        public string title { set; get; }
+    }
+
+    class TabLogger
+    {
+        Process[] chromeTabs = Process.GetProcessesByName("chrome");
+        StreamWriter sw = File.AppendText(@"D:\ChromeLogger.txt");
+        private List<string> jsonObjectList = new List<string>();
+
+
+        public List<string> chromeTabLogger()
+        {
+            if (chromeTabs.Length < 0)
+            {
+                sw.WriteLine("Chrome is not running");
+                sw.Close();
+            }
+            else
+            {
+                foreach (Process p in chromeTabs)
+                {
+                    if (p.MainWindowHandle == IntPtr.Zero)
+                    {
+                        continue;
+                    }
+                    //Find newTab
+                    AutomationElement root = AutomationElement.FromHandle(p.MainWindowHandle);
+                    Condition condNewTab = new PropertyCondition(AutomationElement.NameProperty, "New Tab");
+                    AutomationElement elmNewTab = root.FindFirst(TreeScope.Descendants, condNewTab);
+                    // get the tabstrip by getting the parent of the 'new tab' button 
+                    TreeWalker treewalker = TreeWalker.ControlViewWalker;
+                    AutomationElement elmTabStrip = treewalker.GetParent(elmNewTab);
+                    // loop through all the tabs and get the names which is the page title 
+                    Condition condTabItem = new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.TabItem);
+                    foreach (AutomationElement tabitem in elmTabStrip.FindAll(TreeScope.Children, condTabItem))
+                    {
+                        JSONLOG tab = new JSONLOG { title = tabitem.Current.Name };
+                        string k = Newtonsoft.Json.JsonConvert.SerializeObject(tab);
+                        jsonObjectList.Add(k);
+                    }
+                }
+
+            }            
+            return jsonObjectList;
+        }
+    }
+}
---
+++ at-windows/WindowsLogger/WindowsLogger/ProcessLogger.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+using System.Configuration;
+using System.Diagnostics;
+using System.IO;
+using System.Windows;
+using Newtonsoft.Json;
+
+
+namespace WindowsLogger
+{
+    class JSONProcess
+    {
+        public string processName { get; set; }
+        public string processTitle { get; set; }
+    }
+
+    class ProcessLogger
+    {
+        private Process[] processList = Process.GetProcesses();        
+        private List<string> jsonObjectList = new List<string>();
+
+        
+        public List<string> LogProcess()
+        {
+            foreach (Process p in processList)
+            {                
+                try
+                {
+                    if (p.MainWindowHandle != IntPtr.Zero)
+                    {
+                        JSONProcess pro = new JSONProcess { processName = p.ProcessName, processTitle = p.MainWindowTitle };
+                        string k = Newtonsoft.Json.JsonConvert.SerializeObject(pro);
+                        jsonObjectList.Add(k);
+                    }
+
+                    p.EnableRaisingEvents = true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Errror");
+                }
+                
+            }            
+            return jsonObjectList;
+        }
+    }
+}
